0d08b0cb64f793c73fe63f86abf5891a6df3eca0
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-09-03 22:31:12 +0100

    repository test created

--- src/main/java/com/space/api/AppUserResource.java
+++ src/main/java/com/space/api/AppUserResource.java
@@ -5,6 +5,7 @@ import com.space.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
